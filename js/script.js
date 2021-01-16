5e0bd7f45f10bd5fcc7a650545d0fca3d25e1824
Piotrek <pjablonski123.00@wp.pl>
2021-01-16 16:58:21 +0100

    fix bugs

--- js/script.js
+++ js/script.js
@@ -2,12 +2,11 @@
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 });*/
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
-}
 
-  /* [DONE] remove class 'active' from all article links */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,19 +16,16 @@ const titleClickHandler = function(){
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
 
+  console.log('clickedElement:', clickedElement);
+
   /* [DONE] remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.posts article.active');
+  /* get 'href' attribute from the clicked link */
 
-  for(let activeArticle of activeArticles){
-  	activeArticle.classList.remove('active');
-  }
-
-  /* get 'href' atribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' atribute) */
+  /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+}
 
 const links = document.querySelectorAll('.titles a');
 
